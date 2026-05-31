6afca7f4f60b95d4dca74fa772ba3430354b0db0
gaurav pant <gauravpant870@gmail.com>
2026-05-31 21:44:52 +0545

    mixins

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('students/<int:id>/', views.studentdetailview),
 
     path('employees/', views.EmployeesView.as_view()), # this is for class based view, we need to call as_view() method to convert class based view into function based view
-    path('employees/<int:id>/', views.EmployeeDetailView.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetailView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from asyncio import mixins
+
 from django.shortcuts import render
 from django.http import Http404, JsonResponse
 from students.models import Student
@@ -9,8 +11,8 @@ from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from .serializers import EmployeeSerializer
 from employees.models import Employee
-# Create your views here.
-
+from rest_framework import generics
+from rest_framework import mixins
 
 @api_view(['GET', 'POST'])
 def studentsview(request):
@@ -64,47 +66,72 @@ def studentdetailview(request, id):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 
-class EmployeesView(APIView):
-    def get(self, request):
-        employees = Employee.objects.all()
-        employees_json = EmployeeSerializer(employees, many=True)
-        return Response(employees_json.data, status=status.HTTP_200_OK)
+# class EmployeesView(APIView):  # inherit from APIView to create a class-based view. APIView provides methods for handling HTTP requests and responses, making it easier to create RESTful APIs.
+#     def get(self, request):
+#         employees = Employee.objects.all()
+#         employees_json = EmployeeSerializer(employees, many=True)
+#         return Response(employees_json.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        data = EmployeeSerializer(data=request.data)
-        if data.is_valid():
-            data.save()
-            return Response(data.data, status=status.HTTP_201_CREATED)
-        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         data = EmployeeSerializer(data=request.data)
+#         if data.is_valid():
+#             data.save()
+#             return Response(data.data, status=status.HTTP_201_CREATED)
+#         return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class EmployeeDetailView(APIView):
-    def get_object(self, id):
-        try:
-            return Employee.objects.get(id=id)
-        except Employee.DoesNotExist:
-            raise Http404
+# class EmployeeDetailView(APIView):
+#     def get_object(self, id):
+#         try:
+#             return Employee.objects.get(id=id)
+#         except Employee.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, id):
-        employee = self.get_object(id) # self is used to call another method of the same class 
-        if employee is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        employee_json = EmployeeSerializer(employee)
-        return Response(employee_json.data, status=status.HTTP_200_OK)
+#     def get(self, request, id):
+#         employee = self.get_object(id) # self is used to call another method of the same class 
+#         if employee is None:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         employee_json = EmployeeSerializer(employee)
+#         return Response(employee_json.data, status=status.HTTP_200_OK)
 
-    def put(self, request, id):
-        employee = self.get_object(id) 
-        if employee is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        employee_json = EmployeeSerializer(employee, data=request.data)
-        if employee_json.is_valid():
-            employee_json.save()
-            return Response(employee_json.data, status=status.HTTP_200_OK)
-        else:
-            return Response(employee_json.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, id):
+#         employee = self.get_object(id) 
+#         if employee is None:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         employee_json = EmployeeSerializer(employee, data=request.data)
+#         if employee_json.is_valid():
+#             employee_json.save()
+#             return Response(employee_json.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(employee_json.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, id):
-        employee = self.get_object(id)
-        if employee is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        employee.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, id):
+#         employee = self.get_object(id)
+#         if employee is None:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         employee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# MIXINS
+
+class EmployeesView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+    
+class EmployeeDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk=pk)
+    
+    def put(self, request, pk):
+        return self.update(request, pk=pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk=pk)
